7e8941a2347efce380fc24fa09014bf449c45552
Mickaël Boudaud <mickael.boudaud@maif.fr>
2023-06-21 16:07:04 +0200

    feat: init step 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {join} from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: /~(.+)/,
+                replacement: join(globalThis.process.cwd(), "node_modules/$1"),
+            },
+        ],
+    },
 })
