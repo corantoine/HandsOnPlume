56128187d3a41ed0fea6f9cdf51364675ff52ae7
Mickaël Boudaud <mickael.boudaud@maif.fr>
2023-06-21 16:07:04 +0200

    feat: init step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import initialProducts from './assets/products.json'
+
+import {Badge, BadgeStatus, MediaCard} from "plume-react";
+
+const birthday = new Date(0)
+// ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
+const months = (style = 'long') =>
+    [...Array(12).keys()].map(i => {
+        const date = new Date(birthday)
+        date.setMonth(i)
+        return date.toLocaleDateString('fr-FR', {month: style})
+    })
 
 function App() {
-    const [count, setCount] = useState(0)
+    const products = initialProducts
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <header>
+                <h1>Est-ce bien la saison ?</h1>
+            </header>
+            <main>
+                <div className="products">{products.map((product, i) =>
+                    <MediaCard key={`products-${i}`}
+                               className="products"
+                               media={[product.label.fr, product.emoji].filter(Boolean).join(' ')}
+                               description={<>
+                                   <div className="product-availability-months">{months('narrow').map((month, j) =>
+                                       <Badge key={`product-${i}-availability-months-${j}`}
+                                              status={product.months.includes(j) ? BadgeStatus.success : BadgeStatus.inactive}>
+                                           {month}
+                                       </Badge>)}
+                                   </div>
+                                   <div className="product-some-numbers">
+                                       <div className="product-some-numbers--local">
+                                           {product.local ? '🇫🇷 Local' : '✈️ Non local'}
+                                       </div>
+                                       <div className="product-some-numbers--local">
+                                           <b>{product.CO2 ?? "?"}</b> kgCO2e/kg
+                                       </div>
+                                   </div>
+                               </>
+                               }/>)}
+                </div>
+            </main>
+
         </>
     )
 }
