e437aaae3ea20a45a3fb0990b5b19167b8c71f4f
Mickaël Boudaud <mickael.boudaud@maif.fr>
2023-10-18 17:42:18 +0200

    format some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,44 @@
 import ProductCards from "./productCards.jsx"
+import "./App.scss"
+import initialProducts from "./assets/products.json"
+
 import {useState} from "react"
 import {Button} from "plume-react"
 
-import "./App.scss"
-
-import initialProducts from "./assets/products.json"
 
 function App() {
     const products = initialProducts
     const [addMode, setAddMode] = useState(false)
 
-    return (
-        <>
-            <header>
-                <h1>Est-ce bien la saison ?</h1>
-            </header>
-            <main>
-                {addMode ? (
-                    <>
-                        <h2>Ajouter un Produit</h2>
-                        <span>Intégrer ici un formulaire</span>
-                    </>
-                ) : (
-                    <>
-                        <ProductCards products={products}/>
-                        <div className='products-actions'>
-                            <Button
-                                className='product-add-handler'
-                                primary
-                                onClick={() => setAddMode(true)}
-                            >
-                                Ajouter un produit
-                            </Button>
-                        </div>
-                    </>
-                )}
-            </main>
-            <footer>
-                <div className='love-message'>
-                    Conçu et construit avec tout l&apos;amour du monde par l&apos;équipe
-                    🦚 avec l&apos;aide de nos contributeurs des Coding Days 👨‍💻👨‍💻.
-                </div>
-                <div className='source-message'>
-                    Cette application est fortement inspirée de{" "}
-                    <a href='https://mesfruitsetlegumesdesaison.fr.'>
-                        https://mesfruitsetlegumesdesaison.fr.
-                    </a>
-                </div>
-            </footer>
-        </>
-    )
+    return <>
+        <header>
+            <h1>Est-ce bien la saison ?</h1>
+        </header>
+        <main>{addMode ? (<>
+            <h2>Ajouter un Produit</h2>
+            <span>Intégrer ici un formulaire</span>
+        </>) : (<>
+            <ProductCards products={products}/>
+            <div className='products-actions'>
+                <Button
+                    className='product-add-handler'
+                    primary
+                    onClick={() => setAddMode(true)}
+                >Ajouter un produit</Button>
+            </div>
+        </>)}
+        </main>
+        <footer>
+            <div className='love-message'>
+                Conçu et construit avec tout l&apos;amour du monde par l&apos;équipe
+                🦚 avec l&apos;aide de nos contributeurs des Coding Days 👨‍💻👨‍💻.
+            </div>
+            <div className='source-message'>
+                Cette application est fortement inspirée de
+                <a href='https://mesfruitsetlegumesdesaison.fr.'>https://mesfruitsetlegumesdesaison.fr.</a>
+            </div>
+        </footer>
+    </>
 }
 
 export default App
--- src/useMonthStrings.js
+++ src/useMonthStrings.js
@@ -1,18 +1,18 @@
 const origin = new Date(0)
 // ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
 const months = (style = "long") =>
-	[...Array(12).keys()].map((i) => {
-		const date = new Date(origin)
-		date.setMonth(i)
-		return date.toLocaleDateString("fr-FR", { month: style })
-	})
+    [...Array(12).keys()].map((i) => {
+        const date = new Date(origin)
+        date.setMonth(i)
+        return date.toLocaleDateString("fr-FR", {month: style})
+    })
 
 const useMonthStrings = () => {
-	return {
-		months,
-		narrowMonthStrings: months("narrow"),
-		longMonthStrings: months("long")
-	}
+    return {
+        months,
+        narrowMonthStrings: months("narrow"),
+        longMonthStrings: months("long")
+    }
 }
 
 export default useMonthStrings
