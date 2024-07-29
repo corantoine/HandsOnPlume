2c561a8d2fb6049ff6b12cd6201a597e73795879
BERNIER Mickael <mickael.bernier@maif.fr>
2024-07-29 11:08:51 +0200

    feat: upgrade plume-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,55 @@
-import ProductCards from "./productCards.jsx"
-import "./App.scss"
-import initialProducts from "./assets/products.json"
+import ProductCards from './productCards.jsx'
+import './App.scss'
+import initialProducts from './assets/products.json'
 
-import {useState} from "react"
-import {Button} from "plume-react"
-
+import { useState } from 'react'
+import { Button } from 'plume-react'
 
 function App() {
-    const products = initialProducts
-    const [addMode, setAddMode] = useState(false)
+	const products = initialProducts
+	const [addMode, setAddMode] = useState(false)
 
-    return <>
-        <header>
-            <h1>Est-ce bien la saison ?</h1>
-        </header>
-        <main>{addMode ? (<section>
-                <h2>Ajouter un Produit</h2>
-                <span>Intégrer ici un formulaire</span>
-            </section>) : (<section>
-                <h2>Liste des produits :</h2>
-                <ProductCards products={products}/>
-                <div className='products-actions'>
-                    <Button
-                        className='product-add-handler'
-                        primary
-                        onClick={() => setAddMode(true)}
-                    >Ajouter un produit</Button>
-                </div>
-            </section>)}
-        </main>
-        <footer>
-            <p>Conçu et construit avec tout l'amour du monde par l'équipe 🦚 avec l'aide
-                de nos contributeurs des Coding Days 👨‍💻👨‍💻.
-                <q>Cette application est fortement inspirée de <a
-                    href="https://mesfruitsetlegumesdesaison.fr.">https://mesfruitsetlegumesdesaison.fr.</a></q>
-            </p>
-        </footer>
-    </>
+	return (
+		<>
+			<header>
+				<h1>Est-ce bien la saison ?</h1>
+			</header>
+			<main>
+				{addMode ? (
+					<section>
+						<h2>Ajouter un Produit</h2>
+						<span>Intégrer ici un formulaire</span>
+					</section>
+				) : (
+					<section>
+						<h2>Liste des produits :</h2>
+						<div className='products-actions'>
+							<Button
+								className='product-add-handler'
+								primary
+								onClick={() => setAddMode(true)}
+							>
+								Ajouter un produit
+							</Button>
+						</div>
+						<ProductCards products={products} />
+					</section>
+				)}
+			</main>
+			<footer>
+				<p>
+					Conçu et construit avec tout l'amour du monde par l'équipe 🦚 avec
+					l'aide de nos contributeurs des Coding Days 👨‍💻👨‍💻.
+					<q>
+						Cette application est fortement inspirée de{' '}
+						<a href='https://mesfruitsetlegumesdesaison.fr.'>
+							https://mesfruitsetlegumesdesaison.fr.
+						</a>
+					</q>
+				</p>
+			</footer>
+		</>
+	)
 }
 
 export default App
