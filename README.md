9424697ab8ba4f533c327d66f0b04d660f07a7ab
corantoine <coralie.antoine@maif.fr>
2024-11-13 10:37:06 +0100

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+filter(Boolean)
+convertir en boolean les elems du tableau
+
+Ici, si un label ou un emoji n'est pas présent ça ne plantera pas.
+
+Diff rem / em
+em = se base sur la taille définie de la balise parente
+ rem = se base sur le nav <!-- plutôt utiliser rem -->
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import './App.scss'
-import initialProducts from './assets/products.json'
-
+import "./App.scss";
+import initialProducts from "./assets/products.json";
+import { MediaCard } from "plume-react";
 
 function App() {
-    const products = initialProducts
-
-    return (
-        <>
-            A vos claviers !
-        </>
-    )
+  const products = initialProducts;
+  return (
+    <>
+      <header>
+        <h1>Est-ce bien la saison ?</h1>
+      </header>
+      <main className="product-container">
+        {/* <section> */}
+          {/* <div> */}
+            {products.map((product) => (
+            <MediaCard
+              className="mediacard"
+              key={product.label.fr}
+              title={product.label.fr}
+              media={product.emoji}
+            ></MediaCard>
+            // <MediaCard key={`products-${i}`}
+            // className="product"
+          ))}
+          {/* </div> */}
+        {/* </section> */}
+      </main>
+      <footer> Message plein d'amour </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
